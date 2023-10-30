b44a1fd8fa6300442daa7aca0a75545993b75b90
earbis <jabyruiz@gmail.com>
2023-10-30 12:15:45 +0100

    changes to the general structure

--- avengers/src/main/java/com/example/avengers/MainActivity2.java
+++ avengers/src/main/java/com/example/avengers/MainActivity2.java
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        textView= findViewById(R.id.toolbar);
-        textView.setText("algo");
+        textView= findViewById(R.id.textView);
+        textView.setText("Relative layout");
     }
 }
